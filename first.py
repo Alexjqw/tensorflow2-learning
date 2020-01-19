62e8d83c6de2f4006ba9236ba95084e37000453b
Alexjqw <jiangqiangwei@163.com>
2020-01-19 11:48:21 +0800

    add code

---
+++ first.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+#matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+from tensorflow import keras
+
---
+++ testTensorflow.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+ 
+print(tf.__version__)
+tf.test.is_gpu_available()
